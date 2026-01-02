1a60100b87e4db18132a6b2c4f93b78e44af4d2d
Markel Alvarado <markel.alvarado@gmail.com>
2026-01-02 20:24:21 +0100

    feat: agregar configuracion para multiples colas y limite de capacidad

--- src/main/java/org/cuatrovientos/dam/psp/sincronizacionTaquillasCine/Configuracion.java
+++ src/main/java/org/cuatrovientos/dam/psp/sincronizacionTaquillasCine/Configuracion.java
@@ -3,10 +3,16 @@ package org.cuatrovientos.dam.psp.sincronizacionTaquillasCine;
 public class Configuracion {
     public static final String NOMBRE_CINE = "Cine Clásico";
     public static final int NUMERO_ASIENTOS = 200;
+    
     public static final int NUMERO_TAQUILLAS = 2;
     public static final int TIEMPO_SIMULACION_MINUTOS = 30;
+    
     public static final int TIEMPO_VENTA_MIN_MS = 20000;
     public static final int TIEMPO_VENTA_MAX_MS = 30000;
+    
     public static final int TIEMPO_LLEGADA_MIN_MS = 4000; 
     public static final int TIEMPO_LLEGADA_MAX_MS = 6000;
+    
+    public static final int NUMERO_COLAS = 4;
+    public static final int MAX_PERSONAS_COLA = 10;
 }
